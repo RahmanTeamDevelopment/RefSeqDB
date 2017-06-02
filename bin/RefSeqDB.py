40ff35f57392ae41b250b4f652e0b0a8dd56fe70
Márton Münz <Marton.Muenz@icr.ac.uk>
2017-06-02 19:47:04 +0200

    Minor changes in the interface

--- bin/RefSeqDB.py
+++ bin/RefSeqDB.py
@@ -9,7 +9,7 @@ ver = '0.1.0'
 
 # Command line argument parsing
 descr = 'RefSeqDB v'+ver
-parser = OptionParser(version=ver, description=descr)
+parser = OptionParser(usage='RefSeqDB/env/bin/refseqdb <options>', version=ver, description=descr)
 parser.add_option('-b', default='GRCh37', dest='build', action='store', help="Genome build [default value: %default]")
 parser.add_option('-o', default='output', dest='output', action='store', help="Output file name prefix [default value: %default]")
 (options, args) = parser.parse_args()
